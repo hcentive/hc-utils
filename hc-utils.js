798d356f056832e039bf7a3092c3697499d872ce
Vaibhav Gupta <vaibhav.gupta@hc14-012.local>
2015-08-14 13:55:14 +0530

    Adding encodeuricomponent for request parameter

--- hc-utils.js
+++ hc-utils.js
@@ -11,7 +11,7 @@
         validateMap = {};
     
     var getQueryStringValue = function(key) {
-            return unescape(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+            return encodeURIComponent(unescape(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1")));
         },
         removeItemFromArray = function(arr, item) {
             if (arr && arr.length) {
